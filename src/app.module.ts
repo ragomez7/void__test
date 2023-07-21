6577803dba115a1898a195769d963b9f1f7cb1f2
Rafael Gómez <satrgomez7@gmail.com>
2023-07-21 09:03:26 -0400

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,6 @@ import { UserLeaderboard } from './database/entities/user-leaderboard.entity';
     LeaderboardModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      // url: 'postgres://wnhgiyry:7GYgfg7reb2C0Cg8C8rrozrH5G7Wt6q8@stampy.db.elephantsql.com/wnhgiyry',
       url: process.env.DB_URI,
       entities: [UserLeaderboard],
       synchronize: true,
